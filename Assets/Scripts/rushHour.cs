cd4cb35692abce6058ea0eebab3342eaacb47010
Michael Dellapi <mad758@nyu.edu>
2016-10-13 15:12:59 -0400

    assigned public floats to employee start positions

--- Assets/Scripts/rushHour.cs
+++ Assets/Scripts/rushHour.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class rushHour : MonoBehaviour {
 	public GameObject employee1;
 	public GameObject employee2;
-	public float begin;
+	public float employee1start;
+	public float employee2start;
+
 	public float end;
 	public float moveSpeed;
 	// Use this for initialization
@@ -13,8 +15,8 @@ public class rushHour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		employee1.transform.position = new Vector3 (-6f + Mathf.PingPong (Time.time * moveSpeed, end), transform.position.y, transform.position.z);
-		employee2.transform.position = new Vector3 (6f+Mathf.PingPong (Time.time*moveSpeed, end)*-1f, transform.position.y, -6f);
+		employee1.transform.position = new Vector3 (employee1start + Mathf.PingPong (Time.time * moveSpeed, end), transform.position.y, transform.position.z);
+		employee2.transform.position = new Vector3 (employee2start+Mathf.PingPong (Time.time*moveSpeed, end)*-1f, transform.position.y, -6f);
 	}
 
 
