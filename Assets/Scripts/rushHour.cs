1e781d3577437e00f4fcad7c7e73275cfaebfcc9
Michael Dellapi <mad758@nyu.edu>
2016-10-13 14:31:20 -0400

    used mathf.sin to make employees move more naturally, needs work

---
+++ Assets/Scripts/rushHour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rushHour : MonoBehaviour {
+	public GameObject employee1;
+	public GameObject employee2;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		employee1.transform.Translate (Mathf.Sin(Time.deltaTime), 0f, 0f);
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "wall") {
+			employee1.transform.Translate (-moveSpeed * Time.deltaTime, 0f, 0f);
+			Debug.Log ("ouch");
+		} 
+	}
+}
