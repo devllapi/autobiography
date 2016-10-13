97d01535a7c01da016e0c2aa5a83d66a175827c4
Michael Dellapi <mad758@nyu.edu>
2016-10-13 15:10:49 -0400

    Fixed employee movement script functionality, needs additional polish

--- Assets/Scripts/rushHour.cs
+++ Assets/Scripts/rushHour.cs
@@ -9,13 +9,12 @@ public class rushHour : MonoBehaviour {
 	public float moveSpeed;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		employee1.transform.position = new Vector3 (-6f + Mathf.PingPong (Time.time * moveSpeed, end), transform.position.y, transform.position.z);
-		employee2.transform.position = new Vector3 (-4f+Mathf.PingPong (Time.time*-moveSpeed, end), transform.position.y, transform.position.z);
+		employee2.transform.position = new Vector3 (6f+Mathf.PingPong (Time.time*moveSpeed, end)*-1f, transform.position.y, -6f);
 	}
 
 
